bfe13d19cfbf6d9539861dc25e89028d5c8e07be
Karen Ying <karenying7@gmail.com>
2020-05-15 11:25:21 -0400

    Setup structure

---
+++ src/Components/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/Component.css';
+
+class Component extends React.Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <h1>Hello World!</h1>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Component;
