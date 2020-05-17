54c5c56509c8da8d4963525608f213dd3f90ebaa
Karen Ying <karenying7@gmail.com>
2020-05-17 13:12:37 -0400

    Add hover

--- src/Components/Stack/StackItem.js
+++ src/Components/Stack/StackItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../Styles/Stack/StackItem.css';
-import { IoIosClose } from 'react-icons/io';
+import { IoIosMenu, IoIosClose } from 'react-icons/io';
 import { INPUT_TEXT_GRAY } from '../../Utils/hexConstants';
 
 class StackItem extends React.Component {
@@ -10,13 +10,30 @@ class StackItem extends React.Component {
         const { hex, stop } = this.props;
         return (
             <div className='stackitem-container'>
+                <div className='stackitem-icon-container'>
+                    <div className='stackitem-icon'>
+                        <IoIosMenu
+                            size='25px'
+                            color={INPUT_TEXT_GRAY}
+                            style={{ padding: '5px', marginRight: '10px' }}
+                        />
+                    </div>
+                </div>
                 <input
                     type='text'
                     value={hex}
                     style={{ backgroundColor: hex }}
                 ></input>
                 <input type='number' value={stop}></input>
-                <IoIosClose size='35px' color={INPUT_TEXT_GRAY} />
+                <div className='stackitem-icon-container'>
+                    <div className='stackitem-icon'>
+                        <IoIosClose
+                            size='35px'
+                            color={INPUT_TEXT_GRAY}
+                            style={{ marginLeft: '10px' }}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
