04bf6889a88a4843e87404838c647df9c9f72417
Karen Ying <karenying7@gmail.com>
2020-05-17 09:02:02 -0400

    Change favicon to 16x16 and add logo to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../Styles/Header.css';
+import logo from '../Assets/logo.png';
 
 class Header extends React.Component {
     render() {
         return (
             <div className='header-container'>
+                <img src={logo}></img>
                 <h1>
                     gradient
                     <span
