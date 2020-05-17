6a3ccbd9a251e4dba0e96eddca99eae462ea1390
Karen Ying <karenying7@gmail.com>
2020-05-17 11:05:37 -0400

    Create Gradient class

--- src/Components/Suggestions/Suggestions.js
+++ src/Components/Suggestions/Suggestions.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../Styles/Suggestions/Suggestions.css';
 import SuggestionsItem from './SuggestionsItem';
+import { Gradient } from '../../Utils/Gradient';
 
 class Suggestions extends React.Component {
+    state = {
+        suggestions: [], // gradient objects
+    };
     render() {
-        return <div></div>;
+        return <div className='suggestions-container'></div>;
     }
 }
 
---
+++ src/Utils/Gradient.js
@@ -0,0 +1,8 @@
+class Gradient {
+    constructor(colors, linear) {
+        this.colors = colors; // array of Color objects
+        this.linear = linear; // false = radial
+    }
+}
+
+export { Gradient };
