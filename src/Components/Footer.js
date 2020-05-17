9063b05424daaa8e450b12676067a6916c5182d1
Karen Ying <karenying7@gmail.com>
2020-05-16 21:36:29 -0400

    Setup footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styles/Footer.css';
+
+class Footer extends React.Component {
+    render() {
+        return <div className='footer-container'></div>;
+    }
+}
+
+export default Footer;
